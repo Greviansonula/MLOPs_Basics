80de33b2826cbf424a81f7fc445587f9bbef6aee
Greviansonula <greviansonula@gmail.com>
2023-05-13 20:10:00 +0300

    train step done

---
+++ Week_0_project_setup/train.py
@@ -0,0 +1,32 @@
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+from data import DataModule
+from model import ColaModel
+
+def main():
+    cola_data = DataModule()
+    cola_model = ColaModel()
+    
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="./models", monitor="val_loss", mode="min"
+    )
+    early_stopping_callback = EarlyStopping(
+        monitor="val_loss", patience=3, verbsoe=True, mode="min"
+    )
+    
+    trainer = pl.Trainer(
+        default_root_dir="logs",
+        gpus=(1 if torch.cuda.is_available() else 0),
+        max_epoch=5,
+        fast_dev_run=False,
+        logger=pl.loggers.TensorBoardLogger("logs/", name="cola", version=1),
+        callback=[checkpoint_callback, early_stopping_callback],
+    )
+    trainer.fit(cola_model, cola_data)
+    
+    
+    if __name__ == "__main__":
+        main()
