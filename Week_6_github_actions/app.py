8a239155b34f65bcee5167461c6207a9efb9e997
Grevians Onula <55585900+Greviansonula@users.noreply.github.com>
2023-05-23 11:30:35 +0000

    init week 6

---
+++ Week_6_github_actions/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from inference_onnx import ColaONNXPredictor
+
+predictor = ColaONNXPredictor("./models/model.onnx")
+app =FastAPI(title="MLOPs Basics App")
+
+@app.get("/")
+async def home():
+    return "<h2>This is a sample NLP Project</h2>"
+
+@app.get("/predict")
+async def get_prediction(text: str):
+    return predictor.predict(text)
