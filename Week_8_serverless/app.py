7984fb150f554de4232d8729adcad96faa184e09
Grevians Onula <greviansonula@gmail.com>
2023-08-06 08:42:43 +0000

    update

--- Week_8_serverless/app.py
+++ Week_8_serverless/app.py
@@ -4,9 +4,13 @@ from inference_onnx import ColaONNXPredictor
 predictor = ColaONNXPredictor("./models/model.onnx")
 app =FastAPI(title="MLOPs Basics App")
 
+# @app.get("/")
+# async def home():
+#     return "<h2>This is a sample NLP Project</h2>"
+
 @app.get("/")
-async def home():
-    return "<h2>This is a sample NLP Project</h2>"
+async def get_prediction(text: str):
+    return predictor.predict({"sentence": "This is a samole mes"})
 
 @app.get("/predict")
 async def get_prediction(text: str):
