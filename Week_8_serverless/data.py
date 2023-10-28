f4c3ee65691111ffd0e4fb9eb382d9fde7202695
Grevians Onula <greviansonula@gmail.com>
2023-10-28 16:41:00 +0000

    tokenizer dvc

--- Week_8_serverless/data.py
+++ Week_8_serverless/data.py
@@ -9,7 +9,7 @@ from transformers import AutoTokenizer
 class DataModule(pl.LightningDataModule):
     def __init__(
         self, 
-        model_name="google/owlvit-base-patch32", 
+        model_name="google/bert_uncased_L-2_H-128_A-2", 
         batch_size=64,
         max_length=128,
         ):
@@ -32,6 +32,9 @@ class DataModule(pl.LightningDataModule):
             max_length=512,
         )
         tokenized_input['sentence'] = example['sentence']
+        # Save the tokenizer
+        self.tokenizer.save_pretrained("./tokenizer/")
+
 
         return tokenized_input
         
