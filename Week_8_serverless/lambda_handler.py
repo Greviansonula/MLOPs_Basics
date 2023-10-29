226877fe1fa4e8c07a0e501001be004351621bd6
Grevians Onula <greviansonula@gmail.com>
2023-10-29 15:38:01 +0000

    Fixed tokenizer

--- Week_8_serverless/lambda_handler.py
+++ Week_8_serverless/lambda_handler.py
@@ -1,38 +1,29 @@
-import json
-import logging
-from inference_onnx import ColaONNXPredictor
+"""
+Lambda wrapper
+"""
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+import json
+from inference_onnx import ColaONNXPredictor
 
 inferencing_instance = ColaONNXPredictor("./models/model.onnx")
 
 def lambda_handler(event, context):
-    """
-    Lambda function handler for predicting linguistic acceptability of a given sentence
-    """
-    logger = logging.getLogger()
-    logger.info("Lambda function started")
-
-    try:
-        logger.info("Processing step 1")
-        if "resource" in event.keys():
-            body = event["body"]
-            body = json.loads(body)
-            print(f"Go the input: {body['sentence']}")
-            response = inferencing_instance.predict(body["sentence"])
-            return {
-                "statusCode": 200,
-                "headers": {},
-                "body": json.dumps(response)
-            }
-        else:
-            return inferencing_instance.predict(event["sentence"])
-        logger.info("Lambda function completed successfully")
-    except Exception as e:
-        logger.error(f"An error occurred: {str(e)}")
-        raise
-
+	"""
+	Lambda function handler for predicting linguistic acceptability of the given sentence
+	"""
+	
+	if "resource" in event.keys():
+		body = event["body"]
+		body = json.loads(body)
+		print(f"Got the input: {body['sentence']}")
+		response = inferencing_instance.predict(body["sentence"])
+		return {
+			"statusCode": 200,
+			"headers": {},
+			"body": json.dumps(response)
+		}
+	else:
+		return inferencing_instance.predict(event["sentence"])
 
 if __name__ == "__main__":
 	test = {"sentence": "this is a sample sentence"}
