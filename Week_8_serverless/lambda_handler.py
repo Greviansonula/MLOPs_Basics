f229413d6bc03fea0c84334c915fd2fa16817dd7
Grevians Onula <greviansonula@gmail.com>
2023-10-29 05:09:07 +0000

    Fixed tokenizer

--- Week_8_serverless/lambda_handler.py
+++ Week_8_serverless/lambda_handler.py
@@ -5,7 +5,7 @@ from inference_onnx import ColaONNXPredictor
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-inferencing_instance = ColaONNXPredictor("./models/model.onnx")
+# inferencing_instance = ColaONNXPredictor("./models/model.onnx")
 
 def lambda_handler(event, context):
     """
@@ -20,15 +20,15 @@ def lambda_handler(event, context):
             body = event["body"]
             body = json.loads(body)
             print(f"Go the input: {body['sentence']}")
-            response = inferencing_instance.predict(body["sentence"])
-            return {
-                "statusCode": 200,
-                "headers": {},
-                "body": json.dumps(response)
-            }
-        else:
-            return inferencing_instance.predict(event["sentence"])
-        logger.info("Lambda function completed successfully")
-    except Exception as e:
-        logger.error(f"An error occurred: {str(e)}")
-        raise
+    #         response = inferencing_instance.predict(body["sentence"])
+    #         return {
+    #             "statusCode": 200,
+    #             "headers": {},
+    #             "body": json.dumps(response)
+    #         }
+    #     else:
+    #         return inferencing_instance.predict(event["sentence"])
+    #     logger.info("Lambda function completed successfully")
+    # except Exception as e:
+    #     logger.error(f"An error occurred: {str(e)}")
+    #     raise
